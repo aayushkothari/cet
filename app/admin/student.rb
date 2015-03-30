38a1d41cfc117db1095400bba682555e1d16930b
Aayush Kothari <aayush.kothari10@gmail.com>
2015-03-30 11:30:54 +0530

    add student crud actions to activeadmin

--- app/admin/student.rb
+++ app/admin/student.rb
@@ -13,8 +13,11 @@ ActiveAdmin.register Student do
   #   permitted << :other if resource.something?
   #   permitted
   # end
+  permit_params :name, :sex, :parent_name, :dob, :street_address, :state, :pincode, :ph_landline, :ph_mobile, :email, :class, :school_name, :school_street_address, :school_state, :school_pincode, :school_phone, :school_principal, :aits, :optional_centre_1, :optional_centre_2, :optional_centre_3, :optional_centre_4, :fee_amount, :draft_no, :draft_date, :bank, :branch, :registration_number
+
   index do 
     column :name
+    actions
   end
 
 end
